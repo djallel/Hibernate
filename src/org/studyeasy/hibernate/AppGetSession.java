aece577f109619442a98f8ff5468b3d89ba560ed
TABET Djalal <b_tabet@esi.dz>
2019-11-24 23:59:22 +0100

    updatesession et get Session hibernate

--- src/org/studyeasy/hibernate/AppGetSession.java
+++ src/org/studyeasy/hibernate/AppGetSession.java
@@ -5,35 +5,34 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.studyeasy.hibernate.entity.Users;
 
-public class App {
+public class AppGetSession {
 
   public static void main(String[] args) {
-
+	  
 	  SessionFactory factory = new Configuration()
 			                   .configure("hibernate.cfg.xml")
 			                   .addAnnotatedClass(Users.class)
 			                   .buildSessionFactory();
-
+	  
 	  Session session = factory.getCurrentSession();
-
+	  
 	  try {
 		  // Create object of entity class type
-		  Users user = new Users("username",  "password",  "firstName",  "lastName") ;
+		  Users user = new Users();
 		  // Start transaction
 		  session.beginTransaction();
 		  // Perform operation
-		  session.save(user);
-
-		  // Commit the transaction
+		  user = session.get(Users.class, 0);
+		  // Commit the transaction 
 		  session.getTransaction().commit();
-		  System.out.println("Row added!!!");
-
-
+		  System.out.println(user);
+		  
+		
 	} finally {
 		session.close();
 		factory.close();
 	}
-
+	
 }
 }
 

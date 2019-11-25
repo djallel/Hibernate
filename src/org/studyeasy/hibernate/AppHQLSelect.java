bd38e1fccd93e4349848d07c06674db4a616ea50
TABET DJALAL <b_tabet@esi.dz>
2019-11-25 15:40:21 +0100

    HQL delete select and update

--- src/org/studyeasy/hibernate/AppHQLSelect.java
+++ src/org/studyeasy/hibernate/AppHQLSelect.java
@@ -7,7 +7,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.studyeasy.hibernate.entity.Users;
 
-public class AppHQL {
+public class AppHQLSelect {
 
   public static void main(String[] args) {
 	  
